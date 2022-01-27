6a4163835faac75dad52c86ba81789d154111aa3
Parames Ajhan <parames.a@dia.co.th>
2022-01-27 20:08:29 +0700

    style: 💄 Add Linter and install some plugin

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { render } from "react-dom";
-import "./index.css";
-import { GlobalStyles } from "./global-style";
-import App from "./_app";
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+import { GlobalStyles } from './global-style';
+import App from './_app';
 
 render(
   <>
     <GlobalStyles />
     <App />
   </>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
